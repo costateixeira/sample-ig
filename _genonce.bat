9b68a4e08bf7d70f06341d90560f889203a94e82
Jose Costa Teixeira <jose.a.teixeira@gmail.com>
2020-06-16 21:30:05 +0200

    add wget to online check

--- _genonce.bat
+++ _genonce.bat
@@ -4,13 +4,29 @@ SET input_cache_path=%CD%\input-cache
 
 ECHO Checking internet connection...
 PING tx.fhir.org -4 -n 1 -w 1000 | FINDSTR TTL && GOTO isonline
-ECHO We're offline...
+wget –O icon.png http://tx.fhir.org/favicon.png && del icon.png && GOTO isonline
+ECHO Could not get wget to work or Terminology server is down
+ECHO Make sure PING is working or wget is installed - http://gnuwin32.sourceforge.net/packages/wget.htm
+ECHO
+
+:loop
+choice /t 10 /c yna /cs /d y /n /m "Try to run online anyway? (Yes, No, or Abort)"
+
+if errorlevel 3 goto :loop
+if errorlevel 2 goto :no
+if errorlevel 1 goto :yes
+
+:yes
+ECHO Running with default terminology server
+SET txoption=
+GOTO :isonline
+
+:no
 SET txoption=-tx n/a
-GOTO igpublish
+GOTO :igpublish
+
 
 :isonline
-ECHO We're online
-SET txoption=
 
 :igpublish
 
